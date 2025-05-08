98bbf735ac2eaba2c87846aa9d9db093e0f1ecb8
Zipdox <zipdox@protonmail.com>
2025-05-08 02:16:38 +0200

    Fix for GNOME GitLab

--- background.js
+++ background.js
@@ -30,7 +30,7 @@ function sentHeaders(details) {
     if (!hosts.has(url.host)) return;
     for (const header of details.requestHeaders) {
         if (header.name.toLowerCase() != 'user-agent') continue;
-        header.value = header.value.replaceAll(/mozilla/gi, '');
+        header.value = 'anubis is crap';
     }
     return { requestHeaders: details.requestHeaders };
 }
