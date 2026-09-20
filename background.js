111df4d10fd7a7d475317a49276c086de5960118
andre4ik3 <andre4ik3@fastmail.com>
2026-09-20 11:55:08 +0000

    Port to MV3 and Chromium

--- background.js
+++ background.js
@@ -1,79 +1,98 @@
+// @ts-check
 
-let hosts = new Set();
-browser.storage.local.get('hosts').then(res => {
-    if (res.hosts) {
-        hosts = new Set(res.hosts);
-    }
-});
+const ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+/** @returns {string} */
+function generateUserAgentComponent() {
+  const length = Math.round(Math.random() * 4) + 4;
+  let name = "";
+
+  for (let i = 0; i < length; i++) {
+    name += ALPHABET[Math.floor(Math.random() * ALPHABET.length)];
+  }
+
+  // version
+  if (Math.random() > 0.6) {
+    const version = Array
+      .from({ length: Math.round(Math.random() * 3) + 1 }, () => Math.round(Math.random() * 20))
+      .join(".");
+
+    name += `/${version}`;
+  }
+
+  return name;
+}
+
+/** @returns {string} */
+export function generateUserAgent() {
+  const agent = Array
+    .from({ length: Math.round(Math.random() * 3) + 1 }, generateUserAgentComponent)
+    .join(" ");
+
+  if (agent.toLowerCase().includes("bot")) return generateUserAgent();
+
+  return agent;
+}
+
+/** @param {string} str */
+export function hash(str) {
+  let out = 0x811c9dc5;
+  for (let i = 0; i < str.length; i++) {
+    out ^= str.charCodeAt(i);
+    out = Math.imul(out, 0x01000193);
+  }
+  return out >>> 0;
+}
+
+/** @param {string} host */
+async function hasHost(host) {
+  const rules = await browser.declarativeNetRequest.getDynamicRules();
+  return rules.some((rule) => rule.condition.requestDomains?.includes(host));
+}
+
+/** @param {string} host */
 async function addHost(host) {
-    hosts.add(host);
-    await browser.storage.local.set({
-        hosts: Array.from(hosts)
-    });
+  console.info(`Adding ${host} to hosts`);
+  await browser.declarativeNetRequest.updateDynamicRules({
+    addRules: [{
+      id: hash(host) + 1,
+      condition: {
+        requestDomains: [host],
+        excludedResourceTypes: [],
+      },
+      action: {
+        type: "modifyHeaders",
+        requestHeaders: [{
+          header: "user-agent",
+          operation: "set",
+          value: generateUserAgent(),
+        }],
+      },
+    }],
+  });
 }
 
-browser.runtime.onMessage.addListener(async (host) => {
-    console.info('Adding', host, 'to hosts');
-    await addHost(host);
-});
-
-async function receivedHeaders(details) {
-    const url = new URL(details.url)
-    if (hosts.has(url.host)) return;
-    const cookies = details.responseHeaders.find(header => {
-        return header.name.toLowerCase() == 'set-cookie';
-    });
-    if (!cookies) return;
-    if (!cookies.value.includes('-anubis-')) return;
-    await addHost(url.host);
-    return { redirectUrl: details.url };
-}
-
-function generateUserAgent() {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    const length = Math.round(Math.random() * 16) + 16;
-    let agent = '';
-    for(let i = 0; i < length; i++){
-        if(Math.random() > 0.8){
-            agent += ' ';
-            continue;
-        }
-        agent += alphabet[Math.floor(Math.random() * alphabet.length)];
-    }
-    if(agent.toLowerCase().includes('bot')) return generateUserAgent();
-    return agent;
-}
-
-function sentHeaders(details) {
-    const url = new URL(details.url);
-    if (!hosts.has(url.host)) return;
-    for (const header of details.requestHeaders) {
-        if (header.name.toLowerCase() != 'user-agent') continue;
-        header.value = generateUserAgent();
-    }
-    return { requestHeaders: details.requestHeaders };
+// Chromium needs "extraHeaders" to let us read Set-Cookie, Firefox doesn't and doesn't allow unknown enum cases
+const extraInfoSpec = ["responseHeaders"];
+if (browser.webRequest.OnHeadersReceivedOptions?.EXTRA_HEADERS) {
+  extraInfoSpec.push("extraHeaders");
 }
 
 browser.webRequest.onHeadersReceived.addListener(
-    receivedHeaders,
-    {
-        urls: ['https://*/*'],
-        types: ['main_frame']
-    },
-    [
-        'blocking',
-        'responseHeaders'
-    ]
-);
+  async (details) => {
+    const url = new URL(details.url);
+    console.info(`Handling ${url}`);
+    if (await hasHost(url.host)) return;
 
-browser.webRequest.onBeforeSendHeaders.addListener(
-    sentHeaders,
-    {
-        urls: ['https://*/*']
-    },
-    [
-        'blocking',
-        "requestHeaders"
-    ]
+    const cookies = details.responseHeaders?.find((h) => h.name.toLowerCase() == "set-cookie");
+    if (!cookies?.value?.includes("-anubis-")) return;
+
+    await addHost(url.host);
+    browser.tabs.reload(details.tabId);
+  },
+  {
+    urls: ["*://*/*"],
+    types: ["main_frame"],
+  },
+  extraInfoSpec,
 );
--- scan.js
+++
@@ -1,7 +0,0 @@
-if (document.getElementById('title')?.textContent == "Making sure you're not a bot!") {
-    console.info('Anubis detected!');
-    browser.runtime.sendMessage(document.location.host).then(() => {
-        if(window.location.pathname.startsWith('/.')) return window.history.go(-1);
-        window.location.reload();
-    });
-}
